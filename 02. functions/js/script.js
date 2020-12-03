d515c6ab4326ff6d82e3579aeaab7dc2551feebb
k2 <trofimchuk.an@gmail.com>
2020-12-03 14:09:28 +0200

    Lesson 2

---
+++ 02. functions/js/script.js
@@ -0,0 +1,47 @@
+
+// let a = 10;
+
+// console.log(a);
+
+// let root = document.getElementById("root");
+// console.log("root => ", root);
+// root.innerHTML = "NOT ROOT";
+
+
+// const userAgent = navigator.userAgent;
+// console.log(userAgent); 
+// console.log(navigator)
+// console.log(location)
+// console.log(document)
+// console.log(Object);
+// console.log(window);
+// alert("Text");
+
+
+// function Dec(){
+//     console.log("function declaration");
+// }
+
+
+// const Exp = function(){
+//     console.log("function expression'");
+// }
+
+// const Arrow = () => console.log("Arrow withot params ");
+
+// const Sum = (...params) => {
+//     let sum = 0;
+//     for(let i = 0; i < params.length; i++){
+//         sum += params[i];
+//     }
+//     return sum;
+// }
+
+// (function(){
+//     console.log("Annonymous functions");
+// })();
+
+// console.log(Sum(3,5,7,4,5,34,5,3));
+// Arrow();
+// Exp();
+// Dec();
