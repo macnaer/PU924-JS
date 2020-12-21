acabe67e090165d87fc6d42e6f47b95baeb9501d
master <master@rivne.itstep.org>
2020-12-21 14:29:49 +0200

    Event loop part 2

--- 08. Events part1/js/common.js
+++ 08. Events part1/js/common.js
@@ -56,7 +56,7 @@ div3.addEventListener(
 var input = document.querySelector(".input");
 input.addEventListener('keypress', function(){
     if (event.keyCode < 65 || event.keyCode > 90){
-        event.preventDefault();  // Відмовитись від стандартної поведінки
+        // event.preventDefault();  // Відмовитись від стандартної поведінки
     }
     console.log(event.keyCode);
 });
